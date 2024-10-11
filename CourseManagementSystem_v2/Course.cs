9f385e64fdd064e09d9bc3246b08f76871d1d99d
SafeekMohamed18 <ut03211tic@gmail.com>
2024-10-11 16:45:58 +0530

    que 10,11 done

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    internal class Course
+    {
+        public string CourseId { get; set; }
+        public string title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
